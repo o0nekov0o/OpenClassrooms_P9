2a3a7c15e7901bd1f60b1b4c269821b0ad4b8420
o0nekov0o <nydeanor@gmail.com>
2023-06-12 17:37:44 +0200

    migrations , màj base.html css màj index.html : rendu après login màj nav.html appelé par base.html màj views.py , unversioned files

--- LITReview/feeds/views.py
+++ LITReview/feeds/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
-from django.contrib.auth import get_user_model
+from .models import Ticket, Review
 from django.contrib.auth.decorators import login_required
 
 
 @login_required(login_url='user-login')
 def index(request, *args, **kwargs):
-    users = get_user_model().objects.all()
-    return render(request, "index.html", {'users': users})
+    tickets = Ticket.objects.order_by('-time_created')
+    reviews = Review.objects.all()
+    return render(request, "index.html", {'tickets': tickets, 'reviews': reviews})
 
 
 def register(request, *args, **kwargs):
