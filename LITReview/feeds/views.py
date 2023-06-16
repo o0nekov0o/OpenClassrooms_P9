770a2eec58ae188a6e6d815e9b38d871d0482fb0
o0nekov0o <nydeanor@gmail.com>
2023-06-16 04:09:41 +0200

    frontend + form + màj views.py et index.html ajout des nouvelles entrées créées

--- LITReview/feeds/views.py
+++ LITReview/feeds/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .forms import ReviewForm
+from datetime import datetime
 from .models import Ticket, Review
 from django.contrib.auth.decorators import login_required
 
@@ -7,7 +9,22 @@ from django.contrib.auth.decorators import login_required
 def index(request, *args, **kwargs):
     tickets = Ticket.objects.order_by('-time_created')
     reviews = Review.objects.all()
-    return render(request, "index.html", {'tickets': tickets, 'reviews': reviews})
+    if request.method == "POST":
+        form = ReviewForm(request.POST or None)
+        ticket_id = int(request.POST['ticket'])
+        form.instance.ticket = Ticket.objects.get(id=ticket_id)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.user = request.user
+            review.time_created = datetime.now()
+            form.save()
+        else:
+            print(form.instance.ticket)
+            print(form.errors)
+            print(form.non_field_errors())
+        return render(request, "index.html", {'tickets': tickets, 'reviews': reviews})
+    else:
+        return render(request, "index.html", {'tickets': tickets, 'reviews': reviews})
 
 
 def register(request, *args, **kwargs):
